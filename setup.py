928b30f85db2c5bab65861df3b877031e8bd6da8
Raphaël Doré <rdore@neomailbox.ch>
2017-08-15 14:05:16 -0400

    Added very VERY basic setup.py file. I will need to modify this.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ZimLinksToWaybackMachine',
+    version='0.9',
+    packages=['test'],
+    url='https://github.com/raphaeldore/ZimLinksToWaybackMachine',
+    license='GPLV3',
+    author='Raphael',
+    author_email='',
+    description='Crawl a Zim Wiki and automatically archive links to archive.org'
+)
